63398394d40ac71f308f3ddf5349f5f76a832b1a
Paul Daigle <paul@bitpay.com>
2015-02-12 19:12:21 -0500

    add generate_pem and get_sin_from_pem to key_utils
    The key_utils module is able to generate a pem string and extract a
    correct sin from that pem string.
    
    To Do: make most of these methods private.

---
+++ bitpay/key_utils.py
@@ -0,0 +1,63 @@
+from ecdsa import SigningKey, SECP256k1, VerifyingKey
+import binascii
+import hashlib
+
+def generate_pem():
+  sk = SigningKey.generate(curve=SECP256k1)
+  pem = sk.to_pem()
+  pem = pem.decode("utf-8")
+  return pem
+
+def get_sin_from_pem(pem):
+  public_key = get_compressed_public_key_from_pem(pem)
+  version = get_version_from_compressed_key(public_key)
+  checksum = get_checksum_from_version(version)
+  print(version + checksum)
+  return base58encode(version + checksum)
+
+def base58encode(hexastring):
+  chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+  int_val = int(hexastring, 16)
+  encoded =  encode58("", int_val, chars)
+  print(encoded)
+  return encoded
+
+def encode58(string, int_val, chars):
+  if int_val == 0:
+    return string
+  else:
+    new_val, rem = divmod(int_val, 58) 
+    new_string = (chars[rem]) + string
+    return encode58(new_string, new_val, chars)
+    
+def get_checksum_from_version(version):
+  return sha_digest(sha_digest(version))[0:8]
+
+def get_version_from_compressed_key(key):
+  sh2 = sha_digest(key)
+  rphash = hashlib.new('ripemd160')
+  rphash.update(binascii.unhexlify(sh2))
+  rp1 = rphash.hexdigest()
+  return '0F02' + rp1
+
+def sha_digest(hexastring):
+  return hashlib.sha256(binascii.unhexlify(hexastring)).hexdigest()
+
+def get_compressed_public_key_from_pem(pem):
+  vks = SigningKey.from_pem(pem).get_verifying_key().to_string()
+  bts = binascii.hexlify(vks)
+  compressed = compress_key(bts)
+  return compressed
+
+def compress_key(bts):
+  intval = int(bts, 16)
+  prefix = find_prefix(intval)
+  return prefix + bts[0:64].decode("utf-8")
+
+def find_prefix(intval):
+  if(intval % 2 == 0):
+    prefix = '02'
+  else:
+    prefix = '03'
+  return prefix
+
