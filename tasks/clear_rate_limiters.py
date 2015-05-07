1c4b12733bc767ed34cb16a1afc758cfcf9014ea
Paul Daigle <paul@bitpay.com>
2015-05-07 14:46:08 -0400

    Removed obsolete tasks

--- tasks/clear_rate_limiters.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-print("clearing rate limiters")
-client = MongoClient()
-db = client['bitpay-dev']
-collection = db['ratelimiters']
-collection.remove()
-print("rate limters removed")
--- tasks/clear_server_tokens.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-print("clearing tokens from server")
-client = MongoClient()
-db = client['bitpay-dev']
-collection = db['tokenaccesses']
-collection.remove()
-print("tokens removed")
-
